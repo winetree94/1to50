b765ecc98d26534a7e8508629c2f3b843166ddba
hansol.park <hansol.park@epapyrus.com>
2021-05-26 21:18:48 +0900

    fix layout

--- dist/1to50.e31bb0bc.js
+++ dist/1to50.e31bb0bc.js
@@ -277,7 +277,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42763" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38527" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
